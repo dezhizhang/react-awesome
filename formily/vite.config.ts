42b0f7de0f0d27bba3d12b286a5495a6ab0497c7
dezhizhang <1541609448@qq.com>
2022-07-09 05:47:19 +0800

    feat:update

--- formily/vite.config.ts
+++ formily/vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:[
-      {find:'~',replacement:''}
-    ]
-  },
+  // resolve:{
+  //   alias:[
+  //     // {find:'~',replacement:''}
+  //   ]
+  // },
   plugins: [react({
     jsxRuntime:'classic'
   })],
