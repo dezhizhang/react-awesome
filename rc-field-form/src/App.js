bf1112f554735b54c84ba4e27ebbe15af24421e2
dezhizhang <1541609448@qq.com>
2022-07-09 06:26:15 +0800

    feat:update

---
+++ rc-field-form/src/App.js
@@ -0,0 +1,26 @@
+import Form,{ Field } from "rc-field-form";
+
+
+function App() {
+
+    const handleFinish = (value) => {
+        console.log('value',value);
+        
+    }
+    return <div>
+        <Form
+        initialValues={{username:'',password:''}}
+        onFinish={handleFinish}
+        >
+        <Field name="username">
+            <input placeholder="用户名"/>
+        </Field>
+        <Field name="password">
+        <input placeholder="用户名"/>
+        </Field>
+        <button>提交</button>
+        </Form>
+    </div>
+}
+
+export default App;
