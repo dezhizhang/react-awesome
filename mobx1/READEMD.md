7d7e2d5c8ad736902944cd0ad44f4b15403ef273
dezhizhang <1541609448@qq.com>
2022-08-07 19:58:53 +0800

    mobx

--- mobx1/READEMD.md
+++ mobx1/READEMD.md
@@ -77,3 +77,29 @@ autorun(() => {
 });
 doubler.fetch();
 ```
+### react-mobx
+```js
+import { makeAutoObservable } from 'mobx';
+import { observer } from 'mobx-react';
+
+class Store{
+    number = 1;
+    constructor() {
+        makeAutoObservable(this,{},{autoBind:true})
+    }
+    add() {
+        this.number++;
+    }
+}
+
+const store = new Store();
+
+export default observer(function() {
+    console.log(store);
+    return <div>
+
+        <p>{store.number}</p>
+        <button onClick={store.add}>+</button>
+    </div>
+})
+```
--- mobx1/src/main.jsx
+++ mobx1/src/main.jsx
@@ -5,38 +5,13 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-08-07 16:51:07
  * :last editor: 张德志
- * :date last edited: 2022-08-07 17:48:44
+ * :date last edited: 2022-08-07 19:48:39
  */
-import { observable,autorun,makeAutoObservable,flow,computed } from 'mobx';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import Container from './components/Container';
 
-// let obj = {name:1};
-// let proxyObj = observable(obj);
-
-// autorun(() => {
-//     console.log(proxyObj.name);
-// })
-
-// proxyObj.name = 2;
-// proxyObj.name = 3;
-
-class Doubler {
-    value
-    constructor(value) {
-        makeAutoObservable(this)
-        this.value = value;
-    }
-    get doube() {
-        return this.value *2;
-    }
-    *fetch() {
-        let amout = yield new Promise((resolve) => setTimeout(() => resolve(5),1000));
-        this.value += amout;
-    }
-}
-
-const  doubler = new Doubler(1);
-autorun(() => {
-    console.log(doubler.value);
-});
-doubler.fetch();
-
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <Container/>
+)
