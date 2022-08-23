44b482397ce3c9a1f23bad91e0cd108f31676dd8
dezhizhang <1541609448@qq.com>
2022-08-24 05:16:28 +0800

    feat:ref

--- react1/README.md
+++ react1/README.md
@@ -156,5 +156,48 @@ function App() {
 
 ReactDOM.render(<App/>,document.getElementById('root'))
 ```
+### context
+```js
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const conterContext = React.createContext()
+
+function reducer(state={number:0},action) {
+    switch(action.type) {
+        case 'ADD':
+            return {number:state.number + 1}
+        case 'MINUS':
+            return {number:state.number - 1}
+        default:
+        return state;
+    }
+}
+
+function Counter() {
+   const {state,dispatch} = React.useContext(conterContext)
+    return (
+        <div>
+              <p>{state.number}</p>
+        <button onClick={() =>dispatch({type:'ADD'})}>+</button>
+        <button onClick={() =>dispatch({type:"MINUS"})}>-</button>
+        </div>
+      
+    )
+}
+
+function App() {
+    const [state,dispatch] = React.useReducer(reducer,{number:1})
+    return (
+        <conterContext.Provider value={{state,dispatch}}>
+            <Counter/>
+        </conterContext.Provider>
+    )
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'))
+```
+
 
 
--- react1/src/index.js
+++ react1/src/index.js
@@ -5,32 +5,43 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-08-09 05:21:58
  * :last editor: 张德志
- * :date last edited: 2022-08-23 05:29:19
+ * :date last edited: 2022-08-24 04:32:19
  */
-// import React from "react"
-// import ReactDOM from "react-dom"
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function reducer(state = {number:0},action) {
+
+const conterContext = React.createContext()
+
+function reducer(state={number:0},action) {
     switch(action.type) {
         case 'ADD':
-            return {number:state.number + 1};
+            return {number:state.number + 1}
         case 'MINUS':
             return {number:state.number - 1}
         default:
-            return state
+        return state;
     }
 }
 
-function App() {
-    const [state,dispatch] = React.useReducer(reducer,{number:0});
+function Counter() {
+   const {state,dispatch} = React.useContext(conterContext)
     return (
         <div>
-            <p>{state.number}</p>
-            <button onClick={() => dispatch({type:'ADD'})}>+</button>
-            <button onClick={() => dispatch({type:'MINUS'})}>-</button>
+              <p>{state.number}</p>
+        <button onClick={() =>dispatch({type:'ADD'})}>+</button>
+        <button onClick={() =>dispatch({type:"MINUS"})}>-</button>
         </div>
+      
+    )
+}
+
+function App() {
+    const [state,dispatch] = React.useReducer(reducer,{number:1})
+    return (
+        <conterContext.Provider value={{state,dispatch}}>
+            <Counter/>
+        </conterContext.Provider>
     )
 }
 
