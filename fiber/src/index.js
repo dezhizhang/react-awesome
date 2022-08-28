eb6e9131dcabd530a0e719562dde59d929f85157
dezhizhang <1541609448@qq.com>
2022-08-28 10:37:49 +0800

    feat:update

--- fiber/src/index.js
+++ fiber/src/index.js
@@ -1,17 +1,33 @@
+/*
+ * :file description: 
+ * :name: /fiber/src/index.js
+ * :author: 张德志
+ * :copyright: (c) 2022, Tungee
+ * :date created: 2022-08-28 10:22:21
+ * :last editor: 张德志
+ * :date last edited: 2022-08-28 10:36:58
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const element = (
+  <div id='A1'>
+    <div id='B1'>
+      <div id='C1'></div>
+      <div id='C2'></div>
+    </div>
+    <div id='B2'></div>
+  </div>
+)
+
+console.log(element);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    hello
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
