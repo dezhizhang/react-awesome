e6fd14d77934c0fb673fdcfc7439bbc6393f7fc6
dezhizhang <1541609448@qq.com>
2022-09-13 05:04:17 +0800

    feat:update

--- react-event/src/index.js
+++ react-event/src/index.js
@@ -5,13 +5,25 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-09-03 05:53:20
  * :last editor: 张德志
- * :date last edited: 2022-09-13 04:56:01
+ * :date last edited: 2022-09-13 05:03:00
  */
 import React from './react';
 import ReactDOM from './react-dom';
 
+
+const handleDivClick = () => {
+  console.log('父元素冒泡')
+}
+
+const handleButtonClick = () => {
+  console.log('子元素的冒泡');
+}
+
 let element = (
-  <button>按钮</button>
+  <div  onClick={handleDivClick}>
+    <button onClick={handleButtonClick}>按钮</button>
+  </div>
+  
 )
 
 
