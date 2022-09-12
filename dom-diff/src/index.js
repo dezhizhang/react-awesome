cb8880ffb77f7bae6b0a94bad78a79edef90aa58
dezhizhang <1541609448@qq.com>
2022-09-13 05:16:59 +0800

    feat:初始化

---
+++ dom-diff/src/index.js
@@ -0,0 +1,11 @@
+/*
+ * :file description: 
+ * :name: /dom-diff/src/index.js
+ * :author: 张德志
+ * :copyright: (c) 2022, Tungee
+ * :date created: 2022-09-13 05:09:52
+ * :last editor: 张德志
+ * :date last edited: 2022-09-13 05:14:19
+ */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
