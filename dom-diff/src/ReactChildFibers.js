29fb9624d0cd3251f22e0f670bd5ca231fde6387
dezhizhang <1541609448@qq.com>
2022-09-21 07:06:19 +0800

    feat:fiber

--- dom-diff/src/ReactChildFibers.js
+++ dom-diff/src/ReactChildFibers.js
@@ -5,19 +5,37 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-09-20 07:38:17
  * :last editor: 张德志
- * :date last edited: 2022-09-20 07:47:08
+ * :date last edited: 2022-09-21 07:04:53
  */
 
 import { REACT_ELEMENT_TYPE } from './ReactSymbols';
-
-
+import { createFiberFromElement } from './ReactFilber';
+import { Placement } from './ReactFiberFlag';
 
 function childReconciler(shouldTrackSideEffects) {
+    function placeSingleChild(newFiber) {
+        if(shouldTrackSideEffects && !newFiber.alternate) {
+            newFiber.flags = Placement;
+        }
+        return newFiber;
+    }
+
+    function reconcileSingleElement(returnFiber,currentFirstChild,element) {
+        const created = createFiberFromElement(element);
+        created.return = returnFiber;
+        return created;
+    }
+
+
     return function reconcileChildFibers(returnFiber,currentFirstChild,newChild) {
         const isObject = typeof newChild === 'object' && (newChild);
         if(isObject) {
             switch(newChild.$$typeof) {
                 case REACT_ELEMENT_TYPE:
+                    return placeSingleChild(
+                        reconcileSingleElement(
+                            returnFiber,currentFirstChild,newChild
+                        ))
                     // return placeSingle;
                     default:
                         break;
---
+++ dom-diff/src/ReactFiberFlag.js
@@ -0,0 +1,15 @@
+/*
+ * :file description: 
+ * :name: /dom-diff/src/ReactFiberFlag.js
+ * :author: 张德志
+ * :copyright: (c) 2022, Tungee
+ * :date created: 2022-09-21 07:02:09
+ * :last editor: 张德志
+ * :date last edited: 2022-09-21 07:04:21
+ */
+export const NoFlags = 0b000000000000000000;
+
+export const Placement = 0b000000000000000010;
+
+export const Update = 0b000000000000000100;
+
