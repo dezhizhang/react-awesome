42a925afee0993d4db0cf7588c5d8fd0b11d031f
dezhizhang <1541609448@qq.com>
2022-09-26 06:57:54 +0800

    feat:时间片

--- react-scheduling/src/index.js
+++ react-scheduling/src/index.js
@@ -1,17 +1,19 @@
+/*
+ * :file description: 
+ * :name: /react-scheduling/src/index.js
+ * :author: 张德志
+ * :copyright: (c) 2022, Tungee
+ * :date created: 2022-09-26 05:48:33
+ * :last editor: 张德志
+ * :date last edited: 2022-09-26 06:36:50
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>helo</div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
