03d92b4dc8b2a284f1f933101e2eb686bd1eba80
dezhizhang <1541609448@qq.com>
2022-09-27 06:22:49 +0800

    feat:优先级联列

--- react-scheduling/src/Scheduler.js
+++ react-scheduling/src/Scheduler.js
@@ -5,10 +5,13 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-09-27 05:53:20
  * :last editor: 张德志
- * :date last edited: 2022-09-27 06:00:46
+ * :date last edited: 2022-09-27 06:21:19
  */
 
 let taskIdCounter = 0;
+
+let maxSinged318BitInt = 1073741823;
+
 
 function scheduleCallback(priorityLevel,callback) {
     let currentTime = getCurrentTime();
@@ -27,6 +30,35 @@ function scheduleCallback(priorityLevel,callback) {
         sortIndex:-1,
     }
     newTask.sortIndex = expirationTime;
-    
+
     requestHostCallback(flushWork)
+}
+
+function flushWork() {
+    return workLoop();
+}
+
+
+function workLoop() {
+    currentTask = peek(taskQueue);
+    while(currentTask) {
+        if(currentTask.expirationTime > currentTime &&  shouldYield()) {
+            break
+        }
+        const callback = currentTask.callback;
+        if(typeof callback === 'function') {
+            currentTask.callback = null;
+            const didUserCallbackTimeout = currentTask.expirationTime <= currentTime;
+            const continuationCallback = callback(didUserCallbackTimeout);
+            if(typeof continuationCallback === 'function') {
+                currentTask.callback = continuationCallback;
+            }else {
+                pop(taskQueue);
+            }
+        }else {
+            pop(taskQueue);
+        }
+    }
+    return currentTask;
+    
 }
---
+++ react-scheduling/src/SchedulerPrity.js
@@ -0,0 +1,9 @@
+/*
+ * :file description: 
+ * :name: /react-scheduling/src/SchedulerPrity.js
+ * :author: 张德志
+ * :copyright: (c) 2022, Tungee
+ * :date created: 2022-09-27 06:04:05
+ * :last editor: 张德志
+ * :date last edited: 2022-09-27 06:04:05
+ */
